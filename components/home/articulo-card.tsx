d0e7fa8e58ff86e21c3d358712957c6e4674a96d
xtian7489 <xtian7489@gmail.com>
2025-09-22 10:34:10 -0300

    fix: ajustar clases de diseño en el componente ArticleCard para mejorar la responsividad

--- components/home/articulo-card.tsx
+++ components/home/articulo-card.tsx
@@ -13,10 +13,10 @@ import Link from "next/link";
 export default function ArticleCard({ article, idx }) {
   return (
     <Card
-      className="w-full p-0 flex overflow-hidden  hover:z-10 opacity-80 scale-90 hover:scale-100 hover:opacity-100"
+      className="w-full p-0 flex flex-col-reverse md:flex-row overflow-hidden  hover:z-10 opacity-80 scale-90 hover:scale-100 hover:opacity-100"
       key={article.slug}
     >
-      <CardHeader className="w-1/2 justify-center items-start">
+      <CardHeader className="md:w-1/2 justify-center items-start">
         <CardTitle className="text-start">{article.title}</CardTitle>
         <CardDescription className="text-start">
           {article.description}
@@ -25,7 +25,7 @@ export default function ArticleCard({ article, idx }) {
           <Link href={`articulos/${article.slug}`}>LEER ARTICULO</Link>
         </Button>
       </CardHeader>
-      <CardContent className="p-0 w-1/2">
+      <CardContent className="p-0 md:w-1/2">
         <div className="rounded-t-md">
           <Image
             width={0}
